ccaf595bca0f5ec5bcc40176469288f1326d737d
Samir José <samirjose675@gmail.com>
2022-03-09 20:57:48 -0300

    Netflix Clone by:mihue

--- src/pages/SingUp.js
+++ src/pages/SingUp.js
@@ -23,7 +23,7 @@ const SingUp = () => {
   return (
     <div className={classes.root}>
       <Typography variant="h5" aling="left">
-        Sing In
+        SigIn In
       </Typography>
       <form className={classes.form}>
         <NetflixInput
@@ -41,7 +41,7 @@ const SingUp = () => {
           type="password"
         />
         <NetflixButton onClick={signIn} tpye="submit" radius wide="medium">
-          Sing In
+          SigIn In
         </NetflixButton>
         <Typography variant="subtitle2">
           New to Netflix ?{" "}
